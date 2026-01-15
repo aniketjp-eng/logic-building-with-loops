147bd484cfcbf33d4de1a2e2a505df334d7155af
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-01-15 10:23:40 +0530

    Completed first 10 questions

---
+++ Phase_1/WhileLoop.java
@@ -0,0 +1,129 @@
+// package Phase_1;
+class WhileLoop {
+    public static void main(String []args) {
+//  Print all numbers from 1 down to 10 using a loop -->
+        //     int i = 1;
+        //     while(i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        //    }
+//  Print numbers from 10 down to 1 in reverse order -->
+        // int i = 10;
+        // while(i >=1) {
+        //     System.out.println(i);
+        //     i--;
+        // }
+//  Print all even numbers between 1 and 100 -->
+        // int i = 1;
+        // while(i <= 100) {
+        //     if(i % 2 == 0) {
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        // }
+//  Print all odd numbers between 1 and 100 --> 
+        // int i = 1;
+        // while(i <= 100) {
+        //     if(i % 2 != 0) {
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        // }
+//  Print the multiplication table of a given number from n x 1 to n x 10 --> 
+        // int n = 4, i = 1;
+        // while(i <= 10) {
+        //     System.out.println(n + " * " + i + " = " + n*i);
+        //     i++;
+        // }
+//  Calculate and print the sum of the first n natural numbers --> 
+// method 1 : Formula --> 
+        // int n = 4;
+        // int sum = (n * (n + 1)) / 2;
+        // System.out.println(sum);
+// method 2 : Basic method -->
+        //  int n = 4;
+        //  int i = 1;
+        //  int sum = 0;
+        //  while(i <= n) {
+        //    sum+= i;
+        //    i++;
+        //  }
+        //  System.out.println(sum);
+
+//  Calculate the sum of all even numbers from 1 up to n --> 
+        //  int n = 4;
+        //  int i = 1;
+        //  int sum = 0;
+        //  while(i <= n) {
+        //    if(i %2 == 0) {
+        //      sum +=i;
+        //    }
+        //     i++;
+        //  }
+        //  System.out.println(sum);
+//  Calculate the sum of all odd numbers from 1 up to n --> 
+        //   int n = 4, i = 1, sum = 0;
+        //  while(i <= n) {
+        //     if(i % 2 != 0) {
+        //         sum+= i;
+        //     }
+        //     i++;
+        //  }
+        //  System.out.println(sum);
+//  Calculate and print the factorial of a given number --> 
+        //  increment operator (++)--> 
+        // int i = 1;
+        // int n = 5;
+        // int fact = 1;
+        // while(i <= n) { 
+        //     fact *= i;
+        //     i++;
+        // }
+        // System.out.println(fact);
+        //  decrement operator(--) --> 
+        // int n = 5;
+        // int fact = 1;
+        // while(n >= 1) { 
+        //     fact *= n;
+        //     n--;
+        // }
+        // System.out.println(fact);
+
+//  Find and print the product of all digits of a given number --> 
+        // int n = 111181, prod = 1;
+        // while(n != 0) {
+        //     int lastDigit = n % 10;
+        //     prod *= lastDigit;
+        //     n /= 10;
+        // }
+        // System.out.println(prod);
+
+//  ===============================================================
+    }
+}
+
+
+
+
+//  Count and print the total number of digits in a given number
+//  Reverse the given number and print the reversed value
+//  Check whether the given number is a palindrome
+//  Find and print the sum of digits of the given number
+//  Check whether the given number is an Armstrong number
+//  Check whether the given number is a Perfect number
+//  Print all prime numbers between 1 and 100
+//  Check whether the given number is a prime number
+//  Print the Fibonacci series up to n terms
+//  Find and print the sum of the Fibonacci series up to n terms
+//  Print the square of each number from 1 to n
+//  Print the cube of each number from 1 to n
+//  Print all numbers between a and b that are divisible by 7
+// Print all factors of the given number.
+// Find and print the sum of all factors of the given number.
+// Find the HCF (Highest Common Factor) of two given numbers.
+// Find the LCM (Least Common Multiple) of two given numbers.
+// Find the smallest digit in the given number.
+// Find the largest digit in the given number.
+        
+//     }
+// }
