1bca21f1fcc2011fd7bcf54d43ff43fc6299ca88
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-05-24 19:44:02 +0530

    more practice hard star pattern

--- Phase_1/Pattern Problems/Main.java
+++ Phase_1/Pattern Problems/Main.java
@@ -190,51 +190,241 @@ public class Main {
 //   *       *
 //     *   *
 //       *
-  int N = 10;
-  int n = (N+1)/2;
-  for(int r = 1; r <=N;r++){
-    if(r <=n){
-        // spaces  
-    for(int c = 1; c <= (n-r);c++){
-        System.out.print(" ");
-    }
-    // stars : 
-    for(int c = 1; c <= r;c++){
-        if(c == 1){
-            System.out.print("*");
-        }else {
-            System.out.print(" ");
-        }
-    }
-    for(int c = 1; c <= r-1;c++){
-        if(c == r-1) {
-            System.out.print("*");
-        } else {
-            System.out.print(" ");
-        }
-    }
-    System.out.println();
-    } else {
-        // spaaces :
-        for(int c = 1; c <= r-n;c++){
-        System.out.print(" ");
-    }
-    for(int c = 1; c <= 2*n - (r);c++){
-        if(c == 1){
-            System.out.print("*");
-        }else {
-            System.out.print(" ");
-        }
-    }
-    for(int c = 1; c <= 2*n - (r+1);c++){
-        if(c == 2*n - (r+1)){
-            System.out.print("*");
-        }else {
-            System.out.print(" ");
-        }
-    }
-    System.out.println();
-    }
-  }
+//   int N = 10;
+//   int n = (N+1)/2;
+//   for(int r = 1; r <=N;r++){
+//     if(r <=n){
+//         // spaces  
+//     for(int c = 1; c <= (n-r);c++){
+//         System.out.print(" ");
+//     }
+//     // stars : 
+//     for(int c = 1; c <= r;c++){
+//         if(c == 1){
+//             System.out.print("*");
+//         }else {
+//             System.out.print(" ");
+//         }
+//     }
+//     for(int c = 1; c <= r-1;c++){
+//         if(c == r-1) {
+//             System.out.print("*");
+//         } else {
+//             System.out.print(" ");
+//         }
+//     }
+//     System.out.println();
+//     } else {
+//         // spaaces :
+//         for(int c = 1; c <= r-n;c++){
+//         System.out.print(" ");
+//     }
+//     for(int c = 1; c <= 2*n - (r);c++){
+//         if(c == 1){
+//             System.out.print("*");
+//         }else {
+//             System.out.print(" ");
+//         }
+//     }
+//     for(int c = 1; c <= 2*n - (r+1);c++){
+//         if(c == 2*n - (r+1)){
+//             System.out.print("*");
+//         }else {
+//             System.out.print(" ");
+//         }
+//     }
+//     System.out.println();
+//     }
+//   }
+// -----------------------------------------------------
+// 3. Sandglass Pattern
+// * * * * *
+//   * * * *
+//     * * *
+//       * *
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+        // int N = 8;
+        // int n = (N+1)/2;
+        // for(int r = 1; r <= N; r++){
+        //   if(r <= n) {
+        //       for(int c = 1; c < r;c++){
+        //         System.out.print("  ");
+        //       }
+        //       for(int c = r; c <= n;c++){
+        //         System.out.print("* ");
+        //      }
+
+        //     System.out.println();
+        //   } else {
+        //     for(int c = 1; c <= N-r;c++){
+        //         System.out.print("  ");
+        //       }
+        //       for(int c = 1; c <= r-n+1;c++){
+        //         System.out.print("* ");
+        //      }
+        //       System.out.println();
+
+        //   }
+        // }
+        // --------------------------- 4th Try ---------------------
+      //  int n = 3;
+      //  for(int r = 1; r <=2*n; r++){
+      //   if(r <=n) {
+      //     for(int c = 1; c <=r;c++){
+      //       if(c == 1|| c == r){
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+      //     for(int c = 1; c <= n-r;c++){
+      //       System.out.print(" ");
+      //     }
+      //     for(int c = 1; c <= n-r;c++){
+      //       System.out.print(" ");
+      //     }
+      //       for(int c = 1; c <=r;c++){
+      //       if(c == 1|| c == r){
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+      //     System.out.println();
+
+      //   } else {
+      //     for(int c = 1; c <= (2*n - r +1);c++){
+      //       if(c == 1|| c == 2*n - r +1){
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+
+      //     for(int c = 1; c <= r-(n+1);c++){
+      //       System.out.print(" ");
+      //     }
+      //     for(int c = 1; c <= r-(n+1);c++){
+      //       System.out.print(" ");
+      //     }
+      //       for(int c = 1; c <= (2*n - r +1);c++){
+      //       if(c == 1|| c == 2*n - r +1){
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+      //     System.out.println();
+      //   }
+
+      //  }
+      //  Conclusion : It took me 11 minutes without any book pen i.e dry run
+      // ---------------------- 5th try -------------------------
+      // int n = 4;
+      // for(int r = 1; r <= 2*n; r++){
+      //   if(r <= n) {
+      //     for(int c = 1; c <=r;c++){
+      //       if(c ==1 || c == r) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print("=");
+      //       }
+      //     }
+      //     for(int c = 1; c <= n-r; c++){
+      //        System.out.print("=");
+      //     }
+      //     for(int c = 1; c <= n-r; c++){
+      //        System.out.print("=");
+      //     }
+      //     for(int c = 1; c <=r;c++){
+      //       if(c ==1 || c == r) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print("=");
+      //       }
+      //     }
+      //     System.out.println();
+      //   } else {
+      //     for(int c = 1; c <= (2*n -r)+1; c++){
+      //        if(c ==1 || c == (2*n -r)+1) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print("=");
+      //       }
+      //     }
+      //     for(int c = 1; c <= r-(n+1)  ;c++){
+      //       System.out.print("=");
+      //     }
+      //     for(int c = 1; c <= r-(n+1)  ;c++){
+      //       System.out.print("=");
+      //     }
+      //     for(int c = 1; c <= (2*n -r)+1; c++){
+      //        if(c ==1 || c == (2*n -r)+1) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print("=");
+      //       }
+      //     }
+      //     System.out.println();
+
+      //   }
+      // }
+      // Conclusion : almost 10 minutes still got confused between lower left part logic 
+      // // ---------------- 6th try -----------
+      // int n = 5;
+      // for(int r = 1; r <= 2*n; r++){
+      //   if( r <= n) {
+      //     for(int c = 1; c<=r; c++){
+      //       if( c == 1 || c == r) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+      //     for(int c = 1; c <= n-r;c++){
+      //       System.out.print(" ");
+      //     }
+      //     for(int c = 1; c <= n-r;c++){
+      //       System.out.print(" ");
+      //     }
+      //      for(int c = 1; c<=r; c++){
+      //       if( c == 1 || c == r) {
+      //         System.out.print("*");
+      //       } else {
+      //         System.out.print(" ");
+      //       }
+      //     }
+      //     System.out.println();
+      //   } else {
+      //     for(int c = 1; c<=((2*n)-r)+1;c++) {
+      //       if(c == 1 || c == ((2*n)-r)+1) {
+      //         System.out.print("*");
+      //       } else {
+      //          System.out.print(" ");
+      //       }
+      //     }
+         
+      //     for(int c = 1; c <= r - (n+1); c++){
+      //       System.out.print(" ");
+      //     }
+      //     for(int c = 1; c <= r - (n+1); c++){
+      //       System.out.print(" ");
+      //     }
+      //     for(int c = 1; c<=((2*n)-r)+1;c++) {
+      //       if(c == 1 || c == ((2*n)-r)+1) {
+      //         System.out.print("*");
+      //       } else {
+      //          System.out.print(" ");
+      //       }
+      //     }
+      //      System.out.println();
+
+      //   }
+      // }
+   // Conclusion : almost 13 minutes still got confused between lower left part logic 
     }
 }
